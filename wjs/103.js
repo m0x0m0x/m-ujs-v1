14d12f666d1cc60a8003e39515bfecd0ebd88997
m0x0m0x <mx0mx@tuta.io>
2024-03-06 23:57:48 +0530

    add

--- wjs/103.js
+++ wjs/103.js
@@ -20,6 +20,42 @@ console.log(
   `color:#DD58D6`
 );
 
+console.log(
+  ` %c
+-----------
+1. Start with simple array destructuring
+-----------
+`,
+  "color:#97FFF4;font-size:1rem"
+);
+
+// Manaully extracting
+const numAr1 = [2, 3, 4];
+
+// One by one
+const a = numAr1[0];
+const b = numAr1[1];
+const c = numAr1[2];
+
+console.table(`
+Manual desctruc 
+ - ${a}, ${b}, ${c}
+`);
+
+// Array Destructuring
+
+const [x, y, z] = numAr1;
+console.log(x, y, z);
+
+console.log(
+  ` %c
+-----------
+DC Main Obj
+-----------
+`,
+  "color:#97FFF4;font-size:1rem"
+);
+
 // Example of an italian restaurant
 
 const restaurant = {
@@ -29,7 +65,7 @@ const restaurant = {
   starterMenu: ["Swea", "Pits, Pan"],
   mainMenu: ["Jui, Sti, Smel"],
 };
-
+console.log("Printing it out in table format", "color:green");
 console.table(restaurant);
 console.log(typeof restaurant);
 
