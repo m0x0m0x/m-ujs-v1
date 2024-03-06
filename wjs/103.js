35de371f6776a6d6ba96badcf1e15f7eec8a3e3d
m0x0m0x <mx0mx@tuta.io>
2024-03-06 20:11:26 +0530

    101: Loop thru object

--- wjs/103.js
+++ wjs/103.js
@@ -19,3 +19,22 @@ console.log(
 `,
   `color:#DD58D6`
 );
+
+// Example of an italian restaurant
+
+const restaurant = {
+  name: "Woman Juices",
+  location: "Sweaty Gym Booties",
+  categories: ["Sno, Far, Sca, Pis, Spi"],
+  starterMenu: ["Swea", "Pits, Pan"],
+  mainMenu: ["Jui, Sti, Smel"],
+};
+
+console.table(restaurant);
+console.log(typeof restaurant);
+
+// Code for looping through object taken from mistral
+for (let key in restaurant) {
+  console.log(key, ":", restaurant[key]);
+  console.table(key, ":", restaurant[key]);
+}
