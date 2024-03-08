ab1832b9f1f34bac50818c4a360adb61d7f6ba7b
m0x0m0x <mx0mx@tuta.io>
2024-03-08 19:21:10 +0530

    Attempt exc

--- Ass/ass103.js
+++ Ass/ass103.js
@@ -63,7 +63,30 @@ console.log(thirdBook);
 1.3﻿
 Below is the nested ratings array that contains two other arrays. Destructure the nested ratings arrays into two variables called rating and ratingsCount. In the result of your destructuring, the ratings variable should store a number 4.19, and the ratingsCount variable should store a number 144584.
 */
+console.log(
+  ` 
+  %c 1.3
+  `,
+  "color:orange;font-size:1rem"
+);
+
 const ratings = [
   ["rating", 4.19],
   ["ratingsCount", 144584],
 ];
+console.log(ratings);
+// Solution
+const [[, rating], [, ratingsCount]] = ratings;
+console.table(ratings);
+console.log(ratings);
+
+/*
+1.4
+Below is the ratingStars array. Destructure it into three variables called fiveStarRatings, oneStarRatings and threeStarRatings. Assign the threeStarRatings variable with a default value of 0.
+*/
+console.log(
+  ` 
+  %c 1.4
+  `,
+  "color:orange;font-size:1rem"
+);
