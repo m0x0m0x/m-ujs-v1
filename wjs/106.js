b49c833b1987b5651dc11eba9d1faec979f8dfda
m0x0m0x <mx0mx@tuta.io>
2024-03-12 02:38:53 +0530

    add

--- wjs/106.js
+++ wjs/106.js
@@ -10,3 +10,15 @@ mainHead(`
 106: Spread operator
 - Expand an array into all its elements unpaccking
 `);
+paraText(`
+Since ES6 this spread operator is for unpacking arrays
+`);
+
+subHead("Simple examples");
+const arr = [7, 7, 8];
+
+// Old method of adding alements
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.table(badNewArr);
+
+// Starting
