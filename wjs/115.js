012fbabe4038c7c11b7c1914c6f17be4bb29756d
m0x0m0x <mx0mx@tuta.io>
2024-03-14 03:11:13 +0530

    115

---
+++ wjs/115.js
@@ -0,0 +1,8 @@
+/*
+115: Looping Objects: Object Keys , Values and Entries
+*/
+// === Import Zone ===
+import { mainHead, subHead, paraText } from "./funz.js";
+import { restaurantP2 as re, weekdays as we } from "./rest.js";
+//===
+mainHead("115: Looping Objects: Object Keys , Values and Entries");
