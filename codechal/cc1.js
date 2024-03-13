c8a42c24a1d5d0e5875b59023e47ecb464ab5d10
m0x0m0x <mx0mx@tuta.io>
2024-03-13 17:09:58 +0530

    addd

---
+++ codechal/cc1.js
@@ -0,0 +1,11 @@
+/*  
+Coding Challenge #1
+*/
+// === Import Zone ===
+
+import { mainHead, subHead, paraText } from "../wjs/funz.js";
+// ===
+
+mainHead("111 Coding Challenge #1");
+
+console.log("buty");
