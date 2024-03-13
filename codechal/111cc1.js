e9a6cf364229c69b28c7d3cc5bdeae4c7d9557b7
m0x0m0x <mx0mx@tuta.io>
2024-03-13 17:37:48 +0530

    add

--- codechal/111cc1.js
+++ codechal/111cc1.js
@@ -4,6 +4,12 @@ Coding Challenge #1
 // === Import Zone ===
 
 import { mainHead, subHead, paraText } from "../wjs/funz.js";
+import { game as gPusy } from "./game1.js";
 // ===
 
 mainHead("111 Coding Challenge #1");
+
+subHead("View object");
+console.table(gPusy);
+
+// Solutions Zone
