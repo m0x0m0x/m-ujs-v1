2064efbe754355e1307aa007cacdc5f45fc115e4
m0x0m0x <mx0mx@tuta.io>
2024-03-14 16:42:07 +0530

    setup116c2

--- codechal/116c2.js
+++ codechal/116c2.js
@@ -6,3 +6,8 @@ Coding Challenge #2
 import { mainHead, subHead, paraText } from "../wjs/funz.js";
 import { game as gPusy } from "./game1.js";
 // ===
+
+mainHead("Coding Challenge 2");
+console.table(gPusy);
+
+// 1. Loop Over game scored array and print each player name to the console, along with the goal
