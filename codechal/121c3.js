fdaca9e10e2bc217a31324d5cd6ee6959197120e
null <null>
2024-03-15 17:17:55 +0530

    121

---
+++ codechal/121c3.js
@@ -0,0 +1,11 @@
+/*  
+121: Coding Challenge #3
+*/
+// === Import Zone ===
+import { mainHead, subHead, paraText } from "../wjs/funz.js";
+import { game as gPusy, gameEvents as gE } from "./game1.js";
+// ==
+
+mainHead("121: Coding Challenge #");
+console.log(gPusy);
+console.log(gE);
