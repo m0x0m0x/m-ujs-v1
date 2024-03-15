1fab1520887c5481a7bb1c65c059a300005a8e82
null <null>
2024-03-15 18:11:14 +0530

    122

--- wjs/122_1_2_3.js
+++ wjs/122_1_2_3.js
@@ -3,3 +3,11 @@
 123: Working with strings Part2
 124: Working with strings Part3
 */
+//=== Import zone
+import { mainHead, subHead, paraText } from "./funz.js";
+//
+mainHead(`
+122: Working with strings Part1
+123: Working with strings Part2
+124: Working with strings Part3
+`);
