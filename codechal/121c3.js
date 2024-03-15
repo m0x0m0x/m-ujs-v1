33823e7cd2b04c7c58ef6fdaf89a24a802ad6a85
null <null>
2024-03-15 17:45:44 +0530

    121c3

--- codechal/121c3.js
+++ codechal/121c3.js
@@ -9,3 +9,21 @@ import { game as gPusy, gameEvents as gE } from "./game1.js";
 mainHead("121: Coding Challenge #");
 console.log(gPusy);
 console.log(gE);
+
+//1. Create event with unique events
+subHead("1: Create unique events ");
+
+paraText("Examining Values which we need");
+const eventS = [...new Set(gE.values())];
+console.log(eventS);
+
+//2. Deleting an event
+subHead("2. Removing keys");
+gE.delete(64);
+console.log(gE);
+
+//3. Calvulate Average
+subHead("3. Calculating Average");
+const time = [...gE.keys()].pop();
+console.log(`Average = ${time / gE.size}`);
+console.log(time);
