55cd305b787aae53efb3e559f5c2c9d02681532f
m0x0m0x <mx0mx@tuta.io>
2024-03-17 21:14:12 +0530

    new

--- codechal/125d4.js
+++ codechal/125d4.js
@@ -6,3 +6,11 @@ import { mainHead, subHead, paraText } from "../wjs/funz.js";
 
 mainHead("125: Coding Challegne 4");
 console.log("hey");
+
+//1 . First Problems
+const container = document.querySelector(".text-area"); // Replace with your element's id
+const textarea = document.createElement("textarea");
+container.appendChild(textarea);
+document.querySelector("button").addEventListener("click", function () {
+  const text = document.querySelector("textarea").value;
+});
