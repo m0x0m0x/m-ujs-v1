0a006928488a18e8f68dd09ee7a4ee94f1835f91
m0x0m0x <mx0mx@tuta.io>
2024-03-17 22:12:53 +0530

    with Solution

--- codechal/125d4.js
+++ codechal/125d4.js
@@ -11,6 +11,33 @@ console.log("hey");
 const container = document.querySelector(".text-area"); // Replace with your element's id
 const textarea = document.createElement("textarea");
 container.appendChild(textarea);
+
+/* 
+Strucutre of text that needs to be printed 
+underscore_case 
+ first_name 
+Same_variable
+  calculated_AGE
+delayed_departure 
+*/
+
 document.querySelector("button").addEventListener("click", function () {
   const text = document.querySelector("textarea").value;
+  const rows = text.split("\n");
+  console.log(rows);
+
+  for (const row of rows) {
+    const [first, second] = row.toLocaleLowerCase().trim().split("_");
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(output);
+  }
 });
+
+/// Adding your own key press
+// document.addEventListener("keydown", function (b) {
+//   const text = document.querySelector("textarea").value;
+//   console.log(text);
+// });
