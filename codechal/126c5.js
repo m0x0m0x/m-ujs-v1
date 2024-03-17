de7a996c1c0558c2918545be3dacc7413d7a67fe
m0x0m0x <mx0mx@tuta.io>
2024-03-17 22:33:07 +0530

    add new

---
+++ codechal/126c5.js
@@ -0,0 +1,7 @@
+/*  
+126: String Method Practice 
+*/
+// === Import Zone ===
+import { mainHead, subHead, paraText } from "../wjs/funz.js";
+//===
+mainHead("126: Final Chapter in Section - String Methods Practice");
